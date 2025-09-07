bef53daab72f2223bf550622eca6d4c6bab90780
jayarnim <jayarnim727@gmail.com>
2025-09-08 05:50:18 +0900

    commit

--- loss_fn/kl_div.py
+++ loss_fn/kl_div.py
@@ -1,13 +1,13 @@
 from torch.distributions import kl_divergence
 
 
-def kl_div(posterior, prior, padding=None):
-    kl_tensor = kl_divergence(posterior, prior)
+def kl_div(approx, prior, mask=None):
+    kl_tensor = kl_divergence(approx, prior)
 
-    if padding is not None:
-        padding = padding.to(kl_tensor.device)
-        num_valid = padding.numel() - padding.sum()
-        kl_tensor_masked = kl_tensor.masked_fill(padding, 0.0)
+    if mask is not None:
+        mask = mask.to(kl_tensor.device)
+        num_valid = mask.numel() - mask.sum()
+        kl_tensor_masked = kl_tensor.masked_fill(mask, 0.0)
         kl_sum = kl_tensor_masked.sum()
         return kl_sum / (num_valid + 1e-8)
 
